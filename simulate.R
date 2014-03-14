95b3b941472aab4d8954db85c8c3e859bc836649
Jarad Niemi <niemi@iastate.edu>
2014-03-14 16:30:26 -0500

    normal plot

--- simulate.R
+++ simulate.R
@@ -6,7 +6,7 @@ require(ggplot2)
 rpoint_mass = function(n,zero_prob=.8) ifelse(rbinom(n,1,zero_prob),0,rnorm(n))
 rhorse_shoe = function(n) rnorm(n,0,abs(rt(n,1)))
 
-n = 1000
+n = 45000
 sims = 
   melt(
     data.frame(
@@ -21,4 +21,22 @@ sims =
 
 qplot(x=signal, data=sims, facets=~model)
 
-sims %.% summarise(min(signal),max(signal))
+sims %.% summarise(IQR(signal),max(signal))
+
+
+
+# normal
+y2 = sum(((sims %.% subset(model=="normal"))$y)^2)
+f  = function(x) -n*log(1+x)/2 - y2/(1+x)  /2
+fp = function(x) -n   /(1+x)/2 + y2/(1+x)^2/2
+curve(f,0,2)
+
+samps = ars(1000,f,fp,x=1:10, lb=TRUE)
+hist(samps, 100, prob=TRUE)
+curve(dnorm(x, mean(samps), sd(samps)), add=TRUE, col="red")
+curve(dnorm(x, y2/n-1, y2/n*sqrt(2/n)), add=TRUE, col="blue")
+
+# t-distribution
+f = function(x, y) {
+  sum(dt(y,0,))
+}
